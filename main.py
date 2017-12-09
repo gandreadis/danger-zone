d1b437e720bd949155c5c87312b24891b50e2491
Raffadiely <jackson.matlak@gmail.com>
2017-12-09 15:12:27 +0100

    Added time-limiting options

--- main.py
+++ main.py
@@ -9,16 +9,25 @@ from danger_zone.ui.window_controller import WindowController
 
 def main():
     parser = argparse.ArgumentParser(description='Run traffic simulations.')
-    parser.add_argument('--export', '-e', dest='num_frames', help='Export GIF animation with N frames', action='store',
-                        type=int)
+    parser.add_argument('-gif', dest='gifbool', action='store_true', help='Export GIF animation of the simulation.')
+    parser.add_argument('-limit', metavar='N', dest='time_limit', type=int, help='Run a simulation for N ticks.')
     args = parser.parse_args()
-
-    if args.num_frames:
-        WindowController(Simulation('simple'), GifExporter(args.num_frames))
-        pyglet.app.run()
+    if args.time_limit and args.gifbool:
+        print('Running simulation for %s ticks, then exporting GIF file.' % args.time_limit)
+        WindowController(Simulation('simple', args.time_limit), GifExporter(args.time_limit))
+    elif args.time_limit and not args.gifbool:
+        print('Running simulation for %s ticks.' % args.time_limit)
+        WindowController(Simulation('simple', args.time_limit))
+    elif not args.time_limit and args.gifbool:
+        print('Running simulation for default number of ticks, then exporting GIF file.')
+        WindowController(Simulation('simple',25), GifExporter(25))
+    elif not args.time_limit and not args.gifbool:
+        print('Running simulation for default number of ticks.')
+        WindowController(Simulation('simple',25)) #these 25s can be changed to set the default, or we can figure out something with a parameter
     else:
-        WindowController(Simulation('simple'))
-        pyglet.app.run()
+        print('An error occurred in the options. Blame Jackson if you see this.')
+        return
+    pyglet.app.run()
 
 
 if __name__ == "__main__":
