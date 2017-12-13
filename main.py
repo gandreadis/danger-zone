4a5a38e544305685464fbaf5e39df291ec8b7602
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-12-13 16:49:27 +0100

    Reformat code

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import argparse
-
-import pyglet
-
 import csv
-
 import os
 import pathlib
+
+import pyglet
 
 from danger_zone.parameters import DEFAULT_TIME_LIMIT
 from danger_zone.setups import SETUPS
@@ -16,10 +14,12 @@ from danger_zone.ui.window_controller import WindowController
 
 def main():
     parser = argparse.ArgumentParser(description='Run traffic simulations.')
-    parser.add_argument('-g', '--gif', dest='should_export_gif', action='store_true', help='Export GIF animation of the simulation.')
+    parser.add_argument('-g', '--gif', dest='should_export_gif', action='store_true',
+                        help='Export GIF animation of the simulation.')
     parser.add_argument('-l', '--limit', metavar='N', dest='time_limit', type=int, help='Run a simulation for N ticks.')
     parser.add_argument('-m', '--multiple', metavar='S', dest='runs', type=int, default=1, help='Run S simulations.')
-    parser.add_argument('-c', '--csv', dest='should_export_csv', action='store_true', help='Export CSV file of run statistics.')
+    parser.add_argument('-c', '--csv', dest='should_export_csv', action='store_true',
+                        help='Export CSV file of run statistics.')
     args = parser.parse_args()
 
     total_bicycles_through = 0
@@ -52,18 +52,20 @@ def main():
             print('Running simulation for the default number of ticks.')
             window = WindowController(Simulation(SETUPS["simple-sparse"], DEFAULT_TIME_LIMIT))
         pyglet.app.run()
+
         if args.should_export_csv:
             print('Writing to CSV file...')
-            writer.writerow([window.simulation.bicycles_through, window.simulation.pedestrians_through, window.simulation.cars_through, window.simulation.collision_counter])
+            writer.writerow([window.simulation.bicycles_through, window.simulation.pedestrians_through,
+                             window.simulation.cars_through, window.simulation.collision_counter])
         total_bicycles_through += window.simulation.bicycles_through
         total_pedestrians_through += window.simulation.pedestrians_through
         total_cars_through += window.simulation.cars_through
 
     print('- - - - - - - - - - - - - - - - - - - - - - - - - - -')
     print('      Results:')
-    print('An average of %s bicycles reached their target.' % (total_bicycles_through/args.runs))
-    print('An average of %s pedestrians reached their target.' % (total_pedestrians_through/args.runs))
-    print('An average of %s cars reached their target.' % (total_cars_through/args.runs))
+    print('An average of %s bicycles reached their target.' % (total_bicycles_through / args.runs))
+    print('An average of %s pedestrians reached their target.' % (total_pedestrians_through / args.runs))
+    print('An average of %s cars reached their target.' % (total_cars_through / args.runs))
 
 
 if __name__ == "__main__":
