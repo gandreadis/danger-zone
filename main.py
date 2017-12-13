26a47f2154201fc40fb1258c61824299761d08a7
Raffadiely <jackson.matlak@gmail.com>
2017-12-13 16:29:53 +0100

    Create --csv command to save .csv of results

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ import argparse
 
 import pyglet
 
+import csv
+
+import os
+import pathlib
+
 from danger_zone.parameters import DEFAULT_TIME_LIMIT
 from danger_zone.setups import SETUPS
 from danger_zone.simulation import Simulation
@@ -11,14 +16,23 @@ from danger_zone.ui.window_controller import WindowController
 
 def main():
     parser = argparse.ArgumentParser(description='Run traffic simulations.')
-    parser.add_argument('--gif', '-g', dest='should_export_gif', action='store_true', help='Export GIF animation of the simulation.')
-    parser.add_argument('--limit', '-l', metavar='N', dest='time_limit', type=int, help='Run a simulation for N ticks.')
-    parser.add_argument('--multiple', '-m', metavar='S', dest='runs', type=int, default=1, help='Run S simulations.')
+    parser.add_argument('-g', '--gif', dest='should_export_gif', action='store_true', help='Export GIF animation of the simulation.')
+    parser.add_argument('-l', '--limit', metavar='N', dest='time_limit', type=int, help='Run a simulation for N ticks.')
+    parser.add_argument('-m', '--multiple', metavar='S', dest='runs', type=int, default=1, help='Run S simulations.')
+    parser.add_argument('-c', '--csv', dest='should_export_csv', action='store_true', help='Export CSV file of run statistics.')
     args = parser.parse_args()
 
     total_bicycles_through = 0
     total_pedestrians_through = 0
     total_cars_through = 0
+
+    if args.should_export_csv:
+        pathlib.Path('results').mkdir(exist_ok=True)
+        directory = os.path.join("results", "results.csv")
+        results_file = open(directory, 'w')
+        writer = csv.writer(results_file)
+        writer.writerow(["Bicycles", "Pedestrians", "Cars", "Collisions"])
+
     print('Running %s simulations...' % args.runs)
     for run_number in range(1, args.runs + 1):
         print('- - - - - - - - - - - - - - - - - - - - - - - - - - -')
@@ -38,6 +52,9 @@ def main():
             print('Running simulation for the default number of ticks.')
             window = WindowController(Simulation(SETUPS["simple-sparse"], DEFAULT_TIME_LIMIT))
         pyglet.app.run()
+        if args.should_export_csv:
+            print('Writing to CSV file...')
+            writer.writerow([window.simulation.bicycles_through, window.simulation.pedestrians_through, window.simulation.cars_through, window.simulation.collision_counter])
         total_bicycles_through += window.simulation.bicycles_through
         total_pedestrians_through += window.simulation.pedestrians_through
         total_cars_through += window.simulation.cars_through
